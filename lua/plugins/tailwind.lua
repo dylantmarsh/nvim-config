6ca457b4957bd6a9d36335044c781239b74effb0
Dylan Marsh <dylan@knoware.co>
2024-04-01 19:58:51 -0400

    Updating config

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -18,4 +18,15 @@ return {
       })
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      opts.formatting = {
+        format = require("tailwindcss-colorizer-cmp").formatter,
+      }
+    end,
+  },
 }
