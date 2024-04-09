f6bb776692428722ed66ea4c51b0265e87cc8f22
Dylan Marsh <dylan@knoware.co>
2024-04-09 13:51:49 -0400

    Adding treesitter config

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -2,6 +2,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
       local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "c",
+      "cmake",
+      "cpp",
+      "css",
+      "dockerfile",
+      "glsl",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "make",
+      "markdown",
+      "scss",
+      "toml",
+      "typescript",
+      "vue",
+      "yaml",
+    }
+  }
+}
