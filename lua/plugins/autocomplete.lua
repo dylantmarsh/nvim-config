27a5e5c2ac13f0eacbee53d3c10e1aded952269f
Dylan Marsh <dylan@knoware.co>
2024-04-11 09:58:31 -0400

    Updated dashboard logo and changed autocomplete accept to Tab

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,11 +1,17 @@
 return {
   {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
       local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
         ["<CR>"] = cmp.config.disable,
       })
     end,
